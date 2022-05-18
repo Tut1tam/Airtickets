624aeda4e84bd27ad94b16716b339cd1d0e78f09
Maxim <dolzhich.maxim@yandex.ru>
2022-05-18 14:38:48 +0300

    Initial commit

---
+++ src/test/java/com/example/Airtickets/AirticketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Airtickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirticketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
