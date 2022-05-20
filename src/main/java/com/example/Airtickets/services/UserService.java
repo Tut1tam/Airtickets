07b53e50ec95447fd500b21919973a1488e2938b
Maxim <dolzhich.maxim@yandex.ru>
2022-05-20 05:48:38 +0300

    -admin panel

--- src/main/java/com/example/Airtickets/services/UserService.java
+++ src/main/java/com/example/Airtickets/services/UserService.java
@@ -8,8 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -36,8 +36,26 @@ public class UserService {
     public void banUser(Long id) {
         User user = userRepository.findById(id).orElse(null);
         if (user != null) {
-            user.setActive(false);
-            log.info("Ban user with id = {}; email: {}", user.getId(), user.getEmail());
+            if (user.isActive()) {
+                user.setActive(false);
+                log.info("Ban user with id = {}; email: {}", user.getId(), user.getEmail());
+            } else {
+                user.setActive(true);
+                log.info("Unban user with id = {}; email: {}", user.getId(), user.getEmail());
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
         }
         userRepository.save(user);
     }
