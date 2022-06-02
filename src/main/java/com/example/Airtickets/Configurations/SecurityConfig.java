f7596556078a3f1158aed19876772800da49f303
Maxim <dolzhich.maxim@yandex.ru>
2022-06-02 18:33:43 +0300

    -master

--- src/main/java/com/example/Airtickets/Configurations/SecurityConfig.java
+++ src/main/java/com/example/Airtickets/Configurations/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration")
+                .antMatchers("/", "/ticket/**", "/registration")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
